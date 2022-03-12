a3d9f20d0176551b5f7535bb5a79d142054b453f
Blorgen <39773024+Blorgen@users.noreply.github.com>
2022-03-13 01:07:03 +0400

    Init commit

---
+++ src/test/java/blorgen/shmorgen/urlshorter/UrlShorterApplicationTests.java
@@ -0,0 +1,13 @@
+package blorgen.shmorgen.urlshorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
